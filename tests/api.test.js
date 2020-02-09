723229c7512237f6fd2f3ac146344bd1093a5b57
Otto Pirttimaa <otto.pirttimaa@gmail.com>
2020-02-09 13:54:51 +0200

    Add more api tests for coordinate validation and custom distance parameter

--- tests/api.test.js
+++ tests/api.test.js
@@ -17,14 +17,14 @@ describe('GET unknown endpoint', () => {
 describe('GET /restaurants/search', () => {
     it('returns validation error if query not given', async () => {
         await api.get('/api/restaurants/search')
-            .expect(200, {
+            .expect(400, {
                 error: "Query string 'q' must be atleast 2 characters long"
             })
     })
 
     it('returns validation error if query is too short', async () => {
         await api.get('/api/restaurants/search?q=a')
-            .expect(200, {
+            .expect(400, {
                 error: "Query string 'q' must be atleast 2 characters long"
             })
     })
@@ -34,6 +34,38 @@ describe('GET /restaurants/search', () => {
             .expect(200)
             .then(response => {
                 expect(response.body.restaurants.length).not.toBe(0)
+            })
+    })
+
+    it('returns validation error if latitude is not numeric', async () => {
+        await api.get('/api/restaurants/search?q=burger&lat=text&lon=24.1')
+            .expect(400)
+            .then(response => {
+                expect(response.body.error).toBe('Latitude and longitude values must be numeric')
+            })
+    })
+
+    it('returns validation error if longitude is not numeric', async () => {
+        await api.get('/api/restaurants/search?q=burger&lat=60.2&lon=test')
+            .expect(400)
+            .then(response => {
+                expect(response.body.error).toBe('Latitude and longitude values must be numeric')
+            })
+    })
+
+    it('returns default distance in response', async () => {
+        await api.get('/api/restaurants/search?q=burger&lat=60.2&lon=24.1')
+            .expect(200)
+            .then(response => {
+                expect(response.body.distance).toStrictEqual({ value: 3, unit: 'km' })
+            })
+    })
+
+    it('returns custom distance in response', async () => {
+        await api.get('/api/restaurants/search?q=burger&lat=60.2&lon=24.1&distance=10')
+            .expect(200)
+            .then(response => {
+                expect(response.body.distance).toStrictEqual({ value: 10, unit: 'km' })
             })
     })
 })
